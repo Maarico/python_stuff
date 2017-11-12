e5b8fbb2ec7b3689c0ee7681941c3567ccc9c714
Maarico <marcomarcosimax@gmail.com>
2017-11-12 15:43:38 +0100

    undo

--- cam.py
+++ cam.py
@@ -12,6 +12,6 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ES
+    if key == 27: # exit on ESC
         break
 cv2.destroyWindow("preview")
