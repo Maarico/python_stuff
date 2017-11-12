0f23a7dc487f305fabc6bbde2bfa81e69cbca980
Maarico <marcomarcosimax@gmail.com>
2017-11-12 15:43:11 +0100

    test

--- cam.py
+++ cam.py
@@ -12,6 +12,6 @@ while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27: # exit on ES
         break
 cv2.destroyWindow("preview")
